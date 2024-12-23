1fe4caecf4232a67972c12a11b32232a0ec4f315
soundmoneyprotocol <casmirconsults@gmail.com>
2024-12-23 09:33:23 -0500

    Removed floating pragma

--- contracts/BEZY.sol
+++ contracts/BEZY.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.22;
+pragma solidity 0.8.22;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
